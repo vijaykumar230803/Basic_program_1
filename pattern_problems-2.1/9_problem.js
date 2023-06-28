32f7599409465ea70b75f4d031498996ab96aa36
Vignesh <admin@dreamtusk.com>
2023-06-28 15:58:27 +0530

    9

---
+++ pattern_problems-2.1/9_problem.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+const N = parseInt(prompt("Enter Input NO:"))
+
+let output = ""
+
+let B ="*"
+
+for (let i = N; i >= 1; i--) {
+    
+  for (let j = 1; j <= i; j++) {
+
+    output =output+ j + "  "
+  }
+  output = output+"\n"
+}
+
+console.log(output);
